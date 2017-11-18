10bf73c36fad1b41607a40e2e5faa980de9c89ce
pockinet <33688011+pockinet@users.noreply.github.com>
2017-11-18 21:30:17 +0100

    functions removed, seconds for targettime removed

--- yABC.ino
+++ yABC.ino
@@ -7,144 +7,40 @@ OneWire oneWire(ONE_WIRE_BUS);    //creates oneWire instance
 DallasTemperature sensors(&oneWire);    //creates sensors instance
 
 long startingTime;    //needed for timecounter
-int buzzerPin = 10;   //pin for the buzzer
 long isSeconds;     //actual seconds
 int isMinutes = 0;    //actual minutes
-int targetTime;   //target time
-int cyclestart = 0;   //if "1" cycle starts, if "0" cycle stops
-int overshoot = 1;    //number of °C for heater over- and undershoot
-int heaterPin = 7;    //pin for the heater ssr
-int tempEncoderPinA = 3;    //pin A for the temperature encoder
-int tempEncoderPinB = 4;    //pin B for the temperature encoder
-int targetTemp = 20;    //target temperature
-int tempEncoderPinALast = LOW;    //last state of the temperature encoder pin A
-int x = LOW;    //needed variable for calculating temperature encoder
-int timeEncoderPinA = 5;    //pin A for the time encoder
-int timeEncoderPinB = 6;    //pin B for the time encoder
 int targetMin = 0;    //target minutes
 int targetSec = 0;    //target seconds
-int timeEncoderPinALast = LOW;    //last state of the time encoder pin B
-int y = LOW;    //needed variable for calculating time encoder
+
 int timerStart = 0;   //variable who starts the timer if "1"
-int timeEncoderButtonPin = 8;   //pin for the button on the time encoder
-int tempEncoderButtonPin = 9;   //pin for the button on the temperature encoder
+int cyclestart = 0;   //if "1" cycle starts, if "0" cycle stops
+
+int buzzerPin = 3;   //pin for the buzzer
+int heaterPin = 4;    //pin for the heater ssr
+int tempEncoderPinA = 5;    //pin A for the temperature encoder
+int tempEncoderPinB = 6;    //pin B for the temperature encoder
+int timeEncoderPinA = 7;    //pin A for the time encoder
+int timeEncoderPinB = 8;    //pin B for the time encoder
+int timeEncoderButtonPin = 9;   //pin for the button on the time encoder
+int tempEncoderButtonPin = 10;   //pin for the button on the temperature encoder
+
+int overshoot = 1;    //number of °C for heater overshoot
+int targetTemp = 20;    //target temperature
+
+int tempEncoderPinALast = LOW;    //last state of the temperature encoder pin A
+int timeEncoderPinALast = LOW;    //last state of the time encoder pin B
+int x = LOW;    //needed variable for calculating temperature encoder
+int y = LOW;    //needed variable for calculating time encoder
 
 void setup() {
   sensors.begin();
-  pinMode (tempEncoderPinA, INPUT);
-  pinMode (tempEncoderPinB, INPUT);
   Serial.begin (9600);
   startingTime = millis();
 }
 
-
 void loop() {
-  killCycle();
-  start();
-  readSensor();
-  readTempEncoder();
-  readTimeEncoder();
-  timecalc();
-  timeCounter();
-  heater();
-  screen();
-}
-
-////////////////////////////////////////////////////////////////////////////
-
-//sends actual temperature/target temperatur to serial monitor
-//sends actual minutes:seconds/target minutes:seconds to serial monitor
-void screen(){
-  Serial<<"Temp.: "<<sensors.getTempCByIndex(0)<<" °C"<<"/"<<targetTemp<<" °C"<<endl;
-  Serial<<"Time: "<<isMinutes<<":"<<isSeconds<<"/"<<targetMin<<":"<<targetSec<<endl;
-}
-
-//temperature sensor
-void readSensor(){
-  sensors.requestTemperatures();
-  sensors.getTempCByIndex(0);
-}
-
-//temperature encoder
-void readTempEncoder(){
-  x = digitalRead(tempEncoderPinA);
-  if ((tempEncoderPinALast == LOW) && (x == HIGH)) {
-    if (digitalRead(tempEncoderPinB) == LOW) {
-      targetTemp--;
-    } else {
-      targetTemp++;
-    }
-  }
-  tempEncoderPinALast = x;
-}
-
-//time encoder
-void readTimeEncoder(){
-  y = digitalRead(timeEncoderPinA);
-  if ((timeEncoderPinALast == LOW) && (y == HIGH)) {
-    if (digitalRead(timeEncoderPinB) == LOW) {
-      targetTime--;
-    } else {
-      targetTime++;
-    }
-  }
-  timeEncoderPinALast = y;
-}
-
-//if cycle started and the actual temperature is lower then the target temperatur plus overshoot the heater starts
-//the timer starts
-void heater(){
-  if (sensors.getTempCByIndex(0) < targetTemp + overshoot && cyclestart == 1){
-    digitalWrite(heaterPin, HIGH);
-    timerStart = 1;
-  }
-  else {
-    digitalWrite(heaterPin, LOW);
-  }
-}
-
-//if the temperature encoder is pushed and the time encoder isn't then the cycle starts
-void start(){
-  if (digitalRead(tempEncoderButtonPin) == HIGH && digitalRead(timeEncoderButtonPin) == LOW){
-    cyclestart = 1;
-  }
-}
-
-//calculates actual minutes 
-//calculates actual seconds
-void timeCounter(){
-  if (timerStart == 1){
-    isSeconds = millis() - startingTime;
-    isSeconds = isSeconds / 1000;
-    if (isSeconds == 60){
-      isMinutes++;
-      startingTime = millis();
-    }
-  }
-}
-
-//calculates minutes from target time
-//calculates seconds from target time
-//prevents underflow for minutes and seconds
-void timecalc(){
-  targetSec = targetTime;
-  if (targetTime == 60){
-    targetMin++;
-    targetTime = 0;
-  }
-  if (targetTime < 0){
-    targetMin--;
-    targetTime = 59;
-  }
-  if (targetMin < 0){
-    targetMin = 0;
-  }
-}
-
-//kills the cycle if target time is actual time 
-//kills the cycle if both encoder pins are pushed
-//starts the buzzer for 5 seconds if cycle killed
-void killCycle(){
+  
+//kills the cycle if target time is actual timeor both encoder pins are pushed and starts the buzzer for 5 seconds if cycle killed:
   if (targetMin == isMinutes && targetSec == isSeconds){
     cyclestart = 0;
     digitalWrite (buzzerPin, HIGH);
@@ -160,5 +56,63 @@ void killCycle(){
     delay(5000);
     digitalWrite (buzzerPin, LOW);
   }
-}
+  
+//starts the cycle if the temperature encoder is pushed and the time encoder isn't:
+  if (digitalRead(tempEncoderButtonPin) == HIGH && digitalRead(timeEncoderButtonPin) == LOW){
+    cyclestart = 1;
+  }
+  
+//reads sensors:
+  sensors.requestTemperatures();
+  sensors.getTempCByIndex(0);
+  
+//temperatur encoder reading:
+  x = digitalRead(tempEncoderPinA);
+  if ((tempEncoderPinALast == LOW) && (x == HIGH)) {
+    if (digitalRead(tempEncoderPinB) == LOW) {
+      targetTemp--;
+    } else {
+      targetTemp++;
+    }
+  }
+  tempEncoderPinALast = x;
+  
+//time encoder reading:
+  y = digitalRead(timeEncoderPinA);
+  if ((timeEncoderPinALast == LOW) && (y == HIGH)) {
+    if (digitalRead(timeEncoderPinB) == LOW) {
+      targetMin--;
+    } else {
+      targetMin++;
+    }
+  }
+  timeEncoderPinALast = y;
 
+//calculates minutes from target time and prevents negative values:
+  if (targetMin < 0){
+    targetMin = 0;
+  }
+
+//calculates actual minutes and seconds:  
+  if (timerStart == 1){
+    isSeconds = millis() - startingTime;
+    isSeconds = isSeconds / 1000;
+    if (isSeconds == 60){
+      isMinutes++;
+      startingTime = millis();
+    }
+  }
+
+//starts heater and timer if cycle started && actual temperature is lower then the target temperature + overshoot:
+if (sensors.getTempCByIndex(0) < targetTemp + overshoot && cyclestart == 1){
+    digitalWrite(heaterPin, HIGH);
+    timerStart = 1;
+  }
+  else {
+    digitalWrite(heaterPin, LOW);
+  }
+
+//prints actaul temp, target temp, passed time and target time to the serial monitor:
+  Serial<<"Temp.: "<<sensors.getTempCByIndex(0)<<" °C"<<"/"<<targetTemp<<" °C"<<endl;
+  Serial<<"Time: "<<isMinutes<<":"<<isSeconds<<"/"<<targetMin<<":"<<targetSec<<endl;
+}
